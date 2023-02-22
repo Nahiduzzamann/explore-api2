fc23e7d4ca923df5d64ba61a026d4dc3b1c8421b
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-02-22 17:55:09 +0000

    ok

--- js/json.js
+++ js/json.js
@@ -1,7 +1,21 @@
-const user ={
-    id:1,
-    name:'nahid',
-    job:'developer',
+const user = {
+    id: 1,
+    name: 'nahid',
+    job: 'developer',
 }
 const stringified = JSON.stringify(user);
-console.log(stringified)
+// console.log(stringified)
+
+const shop ={
+    owner :'alia',
+    address:{
+        street:'1234',
+        city:'dhaka',
+        country:'bd'
+    },
+    product :['laptop', 'mic', 'monitor', 'keyboard'],
+    revenue: 45000,
+    isOpen: true,
+    isNew: false,
+}
+console.log(shop)
